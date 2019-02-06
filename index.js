7e5d015e32dce1ca36ed5b708ec7fad42fa32e11
github username <github email address>
2019-02-05 22:27:55 -0800

    Done.

--- index.js
+++ index.js
@@ -12,17 +12,3 @@ function produceDrivingRange(blockRange) {
       };
     }
   }
-
-//     const findDifference = function(startBlock, endBlock) {
-//       if (parseInt(startBlock) > parseInt(endBlock)) {
-//         return parseInt(startBlock) - parseInt(endBlock)
-//       } else {
-//         return parseInt(endBlock) - parseInt(startBlock)
-//       }
-//     }
-//
-//     if (findDifference(startBlock, endBlock) > blockRange) {
-//       return `${(findDifference - blockRange)} blocks out of range`
-//     }
-//   }
-// }
