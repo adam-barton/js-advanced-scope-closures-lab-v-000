9d9f28cb3cc40db0ac7a7b276d1836b36ba489d1
github username <github email address>
2019-02-05 22:27:42 -0800

    Done.

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 function produceDrivingRange(blockRange) {
   return function(startBlock, endBlock) {
 
-    const findDifference = function(startBlock, endBlock) {
-      if (parseInt(startBlock) > parseInt(endBlock)) {
-        return parseInt(startBlock) - parseInt(endBlock)
+    let start = parseInt(startBlock);
+    let end = parseInt(endBlock);
+    let distance = (end - start);
+
+      if (distance > blockRange) {
+        return `${distance - blockRange} blocks out of range`
       } else {
-        return parseInt(endBlock) - parseInt(startBlock)
-      }
-    }
-    
-    if (findDifference(startBlock, endBlock) > blockRange) {
-      return `${(findDifference - blockRange)} blocks out of range`
+        return `within range by ${blockRange - distance}`
+      };
     }
   }
-}
+
+//     const findDifference = function(startBlock, endBlock) {
+//       if (parseInt(startBlock) > parseInt(endBlock)) {
+//         return parseInt(startBlock) - parseInt(endBlock)
+//       } else {
+//         return parseInt(endBlock) - parseInt(startBlock)
+//       }
+//     }
+//
+//     if (findDifference(startBlock, endBlock) > blockRange) {
+//       return `${(findDifference - blockRange)} blocks out of range`
+//     }
+//   }
+// }
